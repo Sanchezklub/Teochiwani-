21e7f2b4734a6b3f6180faca993704a6531a7801
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-01-01 13:18:08 +0100

    q

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBahviourScript : MonoBehaviour
+{
+   
+    bool Otwarta=false;
+
+   
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObcjet.CompareTag("Skrzynia")&& Input.GetKeyDown ( "E") && Otwarta==false)
+        {
+         //   other.sortingOrder=0;
+            Otwarta=true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}

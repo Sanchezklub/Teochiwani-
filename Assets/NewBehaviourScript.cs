7588a8574ed341d15f0d33272d76fcd52e3062ea
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-01-05 21:11:56 +0100

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -6,27 +6,27 @@ public class NewBahviourScript : MonoBehaviour
 {
    
     bool Otwarta=false;
-    Rozbite
+
    
     void Start()
     {
         
     }
     private void OnTriggerEnter2D(Collider2D other)
-    {
-       void OnCollisionEnter(Collision collision)
-    }
+        {
+            void OnCollisionEnter(Collision collision)
+         }
      void OnCollisionEnter(Collision collision)
     {
-        //Check for a match with the specified name on any GameObject that collides with your GameObject
-        if (collision.gameObject.name == "Dzban" && Input.GetKeyDown("mouse[0]") && Rozbite=false)
+       
+        If (collision.gameObject.name == "Dzban" && Input.GetKeyDown("mouse[0]") && Rozbite=false)
         {
             
             Rozbite = true;
         }
 
         //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "MyGameObjectTag")
+        If (collision.gameObject.tag == "MyGameObjectTag")
         {
             //If the GameObject has the same tag as specified, output this message in the console
             Debug.Log("Do something else here");

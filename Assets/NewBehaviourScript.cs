c86658cb070eb8696ddee507821cc19dafc2b545
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-01-05 20:29:09 +0100

    s
    s

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -20,6 +20,22 @@ public class NewBahviourScript : MonoBehaviour
             Otwarta=true;
         }
     }
+     void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Dzban" && Input.GetKeyDown("mouse[0]" && Rozbite=false)
+        {
+            Debug.Log("");
+            Rozbite = true;
+        }
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "MyGameObjectTag")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Do something else here");
+        }
+    }
     // Update is called once per frame
     void Update()
     {

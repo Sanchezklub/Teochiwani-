729aca8aa3e399dd4b679ee3abea04301cce6a8b
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-02-01 19:31:24 +0100

    dodana drabina
    d

---
+++ Assets/SKRYPT/Drabina.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drabina : MonoBehaviour
+{
+    private float speed = 10;
+    private float speed2 = 1;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+
+        }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+
+        }
+        else if (other.tag == "Player")
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+
+        }
+
+
+    }
+
+
+
+
+
+}

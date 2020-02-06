d650d678f31af997ced698fbe18438ed6261121c
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-02-06 22:40:58 +0100

    Atak
    brakuje jeszcze animacji

---
+++ Assets/SKRYPT/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    int currentHealth;
+    void Start()
+    {
+        currentHealth =  MaxHealth;
+    }
+
+   public void TakeDamage(int damage)
+   {
+       currentHealth -=damage;
+        if (currentHealth<=0)
+        {
+            Die();
+
+
+        }
+   }
+
+    void Die ()
+    {
+           GetComponent<Collider2D>().enabled = false;
+           GetComponent<SpriteRenderer>().color =Color.red;
+           this.enabled = false;
+
+    }
+   
+
+
+
+
+
+}

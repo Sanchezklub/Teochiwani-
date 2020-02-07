57b35a86f2622ec8d364a0c4fb9a6dc37c16cec1
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-02-07 16:56:12 +0100

    pasek życia

---
+++ Assets/SKRYPT/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider healthBar;
+    public float MaxHealth=100;
+    public float currentHealth;
+   
+    void Start()
+    {
+        currentHealth=MaxHealth;
+        healthBar.value = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = currentHealth;
+    }
+}

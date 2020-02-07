0fc2dc6bbc00751d9df13af96fdcea5403b37560
gvffdja <59416999+gvffdja@users.noreply.github.com>
2020-02-07 19:41:51 +0100

    oj byczki liczniczki zrobione
    Pozdrawiam Disa nie no zart

---
+++ Assets/SKRYPT/BloodScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        BloodCounterScript.bloodAmount += 1;
+        Destroy (gameObject);
+    }
+
+}

8870bfc69930b0b86ae4c889f82b5e8893163f30
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-02-14 13:04:45 +0100

    MenuPauzy
    -Escapem się pauzuje
    - Jakby ktoś prowadził bug tracker, to jeśli gracz w trakcie pauzy wciśnie spację, to postać gracza wejdzie w pierwszą klatkę animacji skoku i skoczy natychmiast po zakończeniu pauzy
    - Pobierzcie nową wersję Unity

---
+++ Assets/SKRYPT/PauseController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public bool IsPaused;
+    public GameObject PauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!IsPaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        IsPaused = true;
+        Time.timeScale = 0;
+    }
+
+    public void Unpause()
+    {
+        PauseMenuUI.SetActive(false);
+        IsPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Unpause();
+        SceneManager.LoadScene(0);
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}

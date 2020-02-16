339e3d00fca8c456d224b516baba553b8ef1d0b4
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-02-16 22:38:55 +0100

    działające kolce
    zadaja damge i zabijaja

--- Assets/SKRYPT/Health.cs
+++ Assets/SKRYPT/Health.cs
@@ -8,13 +8,23 @@ public class Health : MonoBehaviour
     public Slider healthBar;
     public float MaxHealth=100;
     public float currentHealth;
+    public Vector2 StartingPosition;
    
     void Start()
     {
         currentHealth=MaxHealth;
         healthBar.value = MaxHealth;
     }
-
+     public void TakeDamage(float damage)
+   {
+       currentHealth -=damage;
+        if (currentHealth<=0)
+        {
+            Debug.Log("Dead");
+            Vector2 newPos = new Vector2(StartingPosition.x,StartingPosition.y ); 
+            transform.position = newPos;
+        }
+   }
     // Update is called once per frame
     void Update()
     {
---
+++ Assets/SKRYPT/Spike.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    float damage=100;
+    void OnTriggerEnter2D( Collider2D col)
+    {
+       col.GetComponent<Health>().TakeDamage(damage);
+    }
+}

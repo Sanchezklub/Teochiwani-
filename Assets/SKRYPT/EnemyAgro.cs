748b4c0fc62f6e35a112b05ce740e8f22236d155
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-02-18 11:41:37 +0100

    dodanie AI STEVE

---
+++ Assets/SKRYPT/EnemyAgro.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAgro : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    [SerializeField]
+    float agroRange;
+    [SerializeField]
+    float moveSpeed;
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float distToPlayer = Vector2.Distance(transform.position,player.position);
+        
+        if(distToPlayer <agroRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasingPlayer();
+        }
+    }
+    private void ChasePlayer()
+    {
+        if(transform.position.x<player.position.x)
+        {
+            rb2d.velocity = new Vector2(moveSpeed,0);
+            transform.localScale = new Vector2(3,3);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(-moveSpeed,0);
+            transform.localScale = new Vector2(-3,3);
+        }
+
+    }
+
+    private void StopChasingPlayer()
+    {
+        rb2d.velocity = Vector2.zero;
+    }
+
+
+}

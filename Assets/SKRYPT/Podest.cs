a26f0513f4c2391b46ca1a8afb2dd96efef73f7e
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-03-25 20:56:19 +0100

    podest

---
+++ Assets/SKRYPT/Podest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Podest : MonoBehaviour
+{
+   public BoxCollider2D collider;
+   public LayerMask PlayerMask;
+   public float Range=10f;
+
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.S) )
+        {
+            Debug.Log("JD");
+            collider.isTrigger = true;
+            StartCoroutine(waiter());
+   
+        }
+    }
+    IEnumerator waiter()
+    {
+        //Wait for 2 seconds
+        yield return new WaitForSeconds(1);
+        collider.isTrigger = false;
+    }
+}

72584a142ecf61d61e2f889c6a1f444acaf759da
rjano <59483578+rjano@users.noreply.github.com>
2020-03-28 20:03:48 +0100

    sssspider
    spiderman

---
+++ Assets/SKRYPT/MachineState/SpiderBrain.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBrain : BaseBrain<SpiderBrain>
+{
+    public Animator enemyAnimator;
+    public Transform raycastTransform;
+    public Transform AttackPoint;
+    [SerializeField] public LayerMask WhatIsGround;
+
+
+    public float MaxHealth;
+    public float CurrentHealth;
+    public float damage;
+    public float speed;
+    public float StartFollowDist;
+    public float StopFollowDist;
+    public float StartFightDist;
+    public float AttackRange;
+ 
+    public bool FacingRight;
+
+    private void Start()
+    {
+        //StartPatrol();
+    }
+    private void Update()
+    {
+        UpdateChildState();
+
+        Debug.Log(currentState);
+    }
+
+    public override void ChangeState(BaseState<SpiderBrain> newState)
+    {
+        base.ChangeState(newState);
+
+        currentState?.DeinitState(this);
+        currentState = newState;
+        currentState?.InitState(this);
+    }
+    public override void UpdateChildState()
+    {
+        base.UpdateChildState();
+        currentState?.UpdateState();
+    }
+
+    public void StartShoot()
+    {
+        ChangeState(new SpiderShootState());
+    }
+
+    public void StartJump()
+    {
+        //enemyToFollow = objectToFollow;
+        ChangeState(new SpiderJumpState());
+    }
+
+}
---
+++ Assets/SpiderProjectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderProjectile : MonoBehaviour
+{
+    public float damage;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Health>().TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}

7522ea5e78684c916be6e83eb03b857922a8ac27
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-03-30 08:36:48 +0200

    Save and load part 1

---
+++ Assets/SKRYPT/Architekture/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData 
+{
+    public int level;
+    public int health;
+    public int jumpforce;
+    public int BaseAttackDamage;
+    public int CurrentWeapon;
+    public float[] Position ;
+
+    public PlayerData(Player Player )
+    {
+        level = Player.level ;
+    }
+}
---
+++ Assets/SKRYPT/Architekture/SaveSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+public static class SaveSystem
+{
+    public static void SavePlayer (Player Player)   
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistenDataPath + "/player.fun";
+        FileStream stream  = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(PlayerData);
+
+        formatter.Serialize(stream,data);
+        stream.Close();
+
+    }
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistenDataPath + "/player.fun"
+        if ( File.Exists(path))
+        {
+          BinaryFormatter formatter = new BinaryFormatter(); 
+
+          FileStream stream  = new FileStream(path, FileMode.Open);
+
+          PlayerData data = formatter.Deserialize(stream) as PlayerData;
+          stream.Close();
+          return data;
+        }
+    }
+}

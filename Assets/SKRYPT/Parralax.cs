4c1052e8e90ce56d1d3624c0b124b4e8f18a9aa7
gvffdja <59416999+gvffdja@users.noreply.github.com>
2020-03-31 10:54:35 +0200

    paralaksssa gotowa
    commit me master

--- Assets/SKRYPT/Parralax.cs
+++ Assets/SKRYPT/Parralax.cs
@@ -7,6 +7,7 @@ public class Parralax : MonoBehaviour
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
+    
     void Start()
     {
         startpos = transform.position.x;
@@ -20,7 +21,7 @@ public class Parralax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect) ;
         transform.position = new Vector3 (startpos +dist, transform.position.y, transform.position.z);
-        if (temp >startpos + length) startpos += length;
+        if (temp > startpos + length) startpos += length;
         else if (temp < startpos - length) startpos -= length;
     }
 }

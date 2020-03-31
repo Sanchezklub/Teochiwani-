5a7b598f6d4431fa8572695d1de391c6d94dd099
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-03-31 16:23:42 +0200

    dodanie 2 itemkow

---
+++ Assets/SKRYPT/Items/PodejrzanyGrzyb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodejrzanyGrzyb : BaseItem
+{
+    public Collider2D coll;
+    public string Itemname="Podejrzany Grzyb";
+    public string FlavourText="Jednorożce nie chcą przestać...";
+    public int MaxHealthBuff=10;
+    public int CurrentHealthBuff=10;
+    public override void PickupItem()
+    {
+        coll.enabled=false;
+        Debug.Log("XD");
+        Destroy(gameObject);
+        GameController.instance.DataStorage.PlayerInfo.maxhealth += MaxHealthBuff;
+        GameController.instance.DataStorage.PlayerInfo.currenthealth += CurrentHealthBuff;
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag=="Player")
+        {
+        ShowFloatingText(Itemname);
+        }
+    }
+}
---
+++ Assets/SKRYPT/Items/SkorupaZolwia.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkorupaZolwia : BaseItem
+{
+    public Collider2D coll;
+    public string Itemname="Skorupa żółwia";
+    public string FlavourText="Skała chciałaby być twarda jak to";
+    public int SpeedBuff=10;
+        public override void PickupItem()
+    {
+        coll.enabled=false;
+        Debug.Log("XD");
+        Destroy(gameObject);
+        GameController.instance.DataStorage.PlayerInfo.speed += SpeedBuff;
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag=="Player")
+        {
+        ShowFloatingText(Itemname);
+        }
+    }
+}

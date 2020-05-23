ed42f753788fef11992092ee72db4a104d0f7fba
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-05-23 11:21:43 +0200

    Main menu twix

---
+++ Assets/SKRYPT/Interface/max2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class max2 : MonoBehaviour
+{
+ public float HorizontalSpeed = 1 ;
+ public float MaxHorizontalPosition = 200 ;
+ public float MinHorizontalPosition = -200 ;
+ 
+ // Please, name correctly your variables.
+ // Yourself, in 3 months will be grateful    
+ private RectTransform rectTransform;
+ 
+ void Start ()
+ {
+     rectTransform = GetComponent<RectTransform>();
+ }
+ 
+ void Update ()
+ {
+     Vector2 position = rectTransform.anchoredPosition;
+ 
+     position.x += HorizontalSpeed * Time.unscaledDeltaTime;
+          position.y += HorizontalSpeed/10 * Time.unscaledDeltaTime;
+     if( position.x > MaxHorizontalPosition )
+         position.x = MinHorizontalPosition ;
+ 
+     rectTransform.anchoredPosition = position;
+ }
+}

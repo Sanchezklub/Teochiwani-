98d4d129af5b33372b8b727d27a08812a449756c
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-05-31 17:17:59 +0200

    Slidery głośności cz.1 (sorry że brzydkie)

---
+++ Assets/SKRYPT/UI/MixerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MixerController : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetMasterVolume (float volume)
+    {
+        mixer.SetFloat("MasterVolume", volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        mixer.SetFloat("MusicVolume", volume);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        mixer.SetFloat("SFXVolume", volume);
+    }
+}

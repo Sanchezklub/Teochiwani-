592a398b73e8f5f4daec3ca32a75a1e1ec7a261d
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-05-31 17:46:40 +0200

    Slidery głośności zapisują wartość

--- Assets/SKRYPT/UI/MixerController.cs
+++ Assets/SKRYPT/UI/MixerController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class MixerController : MonoBehaviour
 {
     public AudioMixer mixer;
+
+    public Slider MasterSlider;
+    public Slider MusicSlider;
+    public Slider SFXSlider;
 
     public void SetMasterVolume (float volume)
     {
@@ -18,5 +23,32 @@ public class MixerController : MonoBehaviour
     public void SetSFXVolume(float volume)
     {
         mixer.SetFloat("SFXVolume", volume);
+    }
+
+    private void Start()
+    {
+        MasterSlider.value = PlayerPrefs.GetFloat("MasterVolume", 0);
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0);
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0);
+    }
+
+
+    private void OnDisable()
+    {
+        float MasterVolume = 0;
+        float MusicVolume = 0;
+        float SFXVolume = 0;
+
+        mixer.GetFloat("MasterVolume", out MasterVolume);
+        mixer.GetFloat("MusicVolume", out MusicVolume);
+        mixer.GetFloat("SFXVolume", out SFXVolume);
+
+
+
+        PlayerPrefs.SetFloat("MasterVolume", MasterVolume);
+        PlayerPrefs.SetFloat("MusicVolume", MusicVolume);
+        PlayerPrefs.SetFloat("SFXVolume", SFXVolume);
+        PlayerPrefs.Save();
+
     }
 }

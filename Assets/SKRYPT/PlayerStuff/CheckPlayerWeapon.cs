6b12795f390cdea3207516d37d8ed2785fe1e3f2
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-06-02 15:02:54 +0200

    Gracz się nie ma broni jak wciska nowa gra

---
+++ Assets/SKRYPT/PlayerStuff/CheckPlayerWeapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPlayerWeapon : MonoBehaviour
+{
+    public void CheckPlayerWeaponFunction()
+    {
+        PlayerCombat Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCombat>();
+        if (GameController.instance.DataStorage.PlayerInfo.currentweaponID >= SaveSystem.Instance.Dictionary.WeaponObjects.Length)
+        {
+            BaseWeapon weap = Player.currentWeapon;
+            if (weap != null)
+            {
+                weap.DropWeapon();
+                Destroy(weap.gameObject);
+            }
+        }
+    }
+}

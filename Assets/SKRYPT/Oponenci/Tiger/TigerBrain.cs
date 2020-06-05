25c6364bef3140ed0961b9fbea77350e2031b170
rjano <59483578+rjano@users.noreply.github.com>
2020-06-05 20:03:26 +0200

    Tygrys ai

---
+++ Assets/SKRYPT/Oponenci/Tiger/TigerBrain.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TigerBrain : BaseBrain<TigerBrain>
+{
+    public float AttackRange;
+    public Transform AttackPoint;
+    public Transform raycastTransform;
+    [SerializeField] public LayerMask WhatIsGround;
+    public bool FacingRight;
+    public float damage;
+    public float speed;
+    public float StartFollowDist;
+    public float StopChargeDist;
+    public Animator enemyAnimator;
+    public float ChannellTime;
+    public UnityAction Attacking;
+    public UnityAction LeaveFightState;
+
+    private void Start()
+    {
+        StartPatrol();
+    }
+    private void Update()
+    {
+        UpdateChildState();
+
+        //Debug.Log(currentState);
+    }
+
+    public override void ChangeState(BaseState<TigerBrain> newState)
+    {
+        base.ChangeState(newState);
+
+        currentState?.DeinitState(this);
+        currentState = newState;
+        currentState?.InitState(this);
+    }
+    public override void UpdateChildState()
+    {
+        base.UpdateChildState();
+        currentState?.UpdateState();
+    }
+
+    public void StartPatrol()
+    {
+        ChangeState(new TigerPatrolState());
+    }
+
+    public void StartChannelling()
+    {
+        ChangeState(new TigerChannellingState());
+    }
+
+    public void StartCharge()
+    {
+        //enemyToFollow = objectToFollow;
+        ChangeState(new TigerAtackState());
+    }
+
+    public void ActionAttack()
+    {
+        Attacking?.Invoke();
+    }
+    public void ActionLeaveFightState()
+    {
+        LeaveFightState?.Invoke();
+    }
+}
+
---
+++ Assets/SKRYPT/Oponenci/Tiger/TigerChannellingState.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TigerChannellingState : BaseState<TigerBrain>
+{
+    private TigerBrain brain;
+    Rigidbody2D enemyRigidBody2D;
+    private GameObject player;
+    private float ChannellingTime;
+    public override void InitState(TigerBrain controller)
+    {
+        base.InitState(controller);
+        this.brain = controller;
+
+        player = GameObject.Find("Player");
+        enemyRigidBody2D = brain.GetComponent<Rigidbody2D>();
+        enemyRigidBody2D.velocity = Vector2.zero;
+        FaceTowardsPlayer();
+       
+    }
+
+    void FaceTowardsPlayer()
+    {
+        float PositionDifference = brain.transform.position.x - player.transform.position.x;
+        if (PositionDifference >= 0)
+        {
+            if (brain.FacingRight)
+            {
+                Flip();
+            }
+        }
+        else if (PositionDifference <= 0)
+        {
+            if (!brain.FacingRight)
+            {
+                Flip();
+            }
+        }
+    }
+    public override void UpdateState()
+    {
+        TimeCheck();
+    }
+
+    private void TimeCheck()
+    {
+        ChannellingTime += Time.deltaTime;
+        if (ChannellingTime > brain.ChannellTime)
+        {
+            brain.StartCharge();
+        }
+    }
+
+
+    void Flip()
+    {
+        brain.transform.Rotate(new Vector2(0f, 180f));
+        brain.FacingRight = !brain.FacingRight;
+    }
+    public override void DeinitState(TigerBrain controller)
+    {
+        base.DeinitState(controller);
+    }
+}

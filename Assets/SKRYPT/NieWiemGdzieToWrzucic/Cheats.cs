02328273748429dd44ac6dc39151acaf08740682
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-07-20 14:35:14 +0200

    Przeciwnicy powinni umierać raz + pierwszy czit (jak się trzyma p i wciska 1 to +100 życia)

---
+++ Assets/SKRYPT/NieWiemGdzieToWrzucic/Cheats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                GameController.instance.DataStorage.PlayerInfo.maxhealth += 100;
+                GameController.instance.DataStorage.PlayerInfo.currenthealth += 100;
+            }
+        }        
+    }
+}

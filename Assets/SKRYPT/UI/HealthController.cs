7169c5c02c7cc276cfc885a41517430ff34fd033
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-07-21 22:11:40 +0200

    Efekt z ucinaniem paska życia działa (psuje się jak maxhealth nie jest 100)

--- Assets/SKRYPT/UI/HealthController.cs
+++ Assets/SKRYPT/UI/HealthController.cs
@@ -11,7 +11,7 @@ public class HealthController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        EventController.instance.playerEvents.OnPlayerReceiveDamage += OnDamageTaken;
+        EventController.instance.playerEvents.OnPlayerReceiveDamage += CutEffect;
     }
 
     // Update is called once per frame
@@ -20,13 +20,13 @@ public class HealthController : MonoBehaviour
         
     }
 
-    public void OnDamageTaken(float DamageTaken, float HealthLeft)
+    public void CutEffect(float DamageTaken, float HealthLeft)
     {
         Debug.Log("HealthController :: OnDamageTakenCalled");
         GameObject DamagedBar = Instantiate(CutHealth, transform);
         RectTransform DamagedBarRect = DamagedBar.GetComponent<RectTransform>();
-        //DamagedBarRect.sizeDelta = new Vector2((DamageTaken / GameController.instance.DataStorage.PlayerInfo.maxhealth) * 250, DamagedBarRect.sizeDelta.y);
-        DamagedBarRect.anchoredPosition = new Vector2(Mathf.Lerp(-200, 50, DamageTaken / GameController.instance.DataStorage.PlayerInfo.maxhealth), DamagedBarRect.anchoredPosition.y);
+        DamagedBarRect.sizeDelta = new Vector2((DamageTaken / GameController.instance.DataStorage.PlayerInfo.maxhealth) * 250, DamagedBarRect.sizeDelta.y);
+        DamagedBarRect.anchoredPosition = new Vector2(Mathf.Lerp(-200, 50, ((DamageTaken + GameController.instance.DataStorage.PlayerInfo.currenthealth) / GameController.instance.DataStorage.PlayerInfo.maxhealth)), DamagedBarRect.anchoredPosition.y);
         DamagedBar.SetActive(true);
     }
 }

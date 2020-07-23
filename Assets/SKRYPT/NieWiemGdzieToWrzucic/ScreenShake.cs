179631fc224cceedcd28db1ef7cdac1743cd3f59
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-07-23 12:39:17 +0200

    Nie działający screenshake (nie ogarniam IEnumerator i Coroutine)

---
+++ Assets/SKRYPT/NieWiemGdzieToWrzucic/ScreenShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ScreenShake : MonoBehaviour
+{
+
+    [SerializeField] CinemachineVirtualCamera cam;
+    private CinemachineBasicMultiChannelPerlin noise;
+    private void Start()
+    {
+        noise = cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    public IEnumerator ShakeScreen(float magnitude, float time)
+    {
+        Debug.Log("ShakeStarted");
+        noise.m_AmplitudeGain = magnitude;
+        yield return new WaitForSeconds(time);
+        noise.m_AmplitudeGain = 0;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ShakeScreen(1.5f, 1f);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            ShakeScreen(0.4f, 3);
+        }
+    }
+
+}

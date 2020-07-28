a8c4c58d3465d1eeedd491e158332459491bf21c
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-07-28 13:38:28 +0200

    SpawnBlock naprawiony (chyba)

--- Assets/SKRYPT/LevelGenerator/SpawnBlock.cs
+++ Assets/SKRYPT/LevelGenerator/SpawnBlock.cs
@@ -113,17 +113,17 @@ public class SpawnBlock : MonoBehaviour
              spawnRotation = Quaternion.Euler(0,0,0);
              return;
          }
-        if (Hit[1]==true && Hit[3]==false && Hit[5]==true && Hit[7]==true)
+         else if (Hit[1]==true && Hit[3]==false && Hit[5]==true && Hit[7]==true)
          {
              spawnRotation = Quaternion.Euler(0,0,90);
              return;
          }
-         if (Hit[1]==true && Hit[3]==true && Hit[5]==false && Hit[7]==true)
+          else if (Hit[1]==true && Hit[3]==true && Hit[5]==false && Hit[7]==true)
          {
              spawnRotation = Quaternion.Euler(0,0,180);
              return;
          }
-         if (Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==false)
+         else if (Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==false)
          {
              spawnRotation = Quaternion.Euler(0,0,270);
              return;
@@ -134,39 +134,76 @@ public class SpawnBlock : MonoBehaviour
     }
     private void Wektors()
     {
-        if (Physics2D.Raycast(transform.position,Vector3.up , range,floor ))
-        Hit[1]=true;
+        if (Physics2D.Raycast(transform.position,Vector3.up , range,floor))
+        {
+            Hit[1]=true;
+        }
         else
-        Hit[1]=false;
+        {
+            Hit[1]=false;
+        }
+        if (Physics2D.Raycast(transform.position, myVector = new Vector3(0.5f, 0.5f, 0.0f), range, floor))
+        {
+            Hit[2] = true;
+        }
+        else
+        {
+            Hit[2] = false;
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.right, range, floor))
+        {
+            Hit[3] = true;
+        }
+        else
+        {
+            Hit[3] = false; 
+        }
+        if (Physics2D.Raycast(transform.position, myVector1 = new Vector3(0.5f, -0.5f, 0.0f), range, floor))
+        {
+            Hit[4] = true;
+        }
+        else
+        {
+            Hit[4] = false;
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.down, range, floor))
+        {
+            Hit[5] = true;
+        }
+        else
+        {
+            Hit[5] = false;
+        }
+        if (Physics2D.Raycast(transform.position, myVector2 = new Vector3(-0.5f, -0.5f, 0.0f), range, floor))
+        {
+            Hit[6] = true;
+        }
+        else
+        {
+            Hit[6] = false;
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.left, range, floor))
+        {
+            Hit[7] = true;
+        }
+        else
+        {
+            Hit[7] = false;
+        }
+        if (Physics2D.Raycast(transform.position, myVector3 = new Vector3(-0.5f, 0.5f, 0.0f), range, floor))
+        {
+            Hit[8] = true;
+        }
+        else
+        {
+            Hit[8] = false;
+        }
 
-        if (Physics2D.Raycast(transform.position, myVector = new Vector3(0.5f, 0.5f, 0.0f), range,floor ))
-        Hit[2]=true;
-         else
-        Hit[2]=false;
-        if (Physics2D.Raycast(transform.position,Vector3.right , range,floor ))
-        Hit[3]=true;
-         else
-        Hit[3]=false;
-        if (Physics2D.Raycast(transform.position, myVector1 = new Vector3(0.5f, -0.5f, 0.0f) , range,floor ))
-        Hit[4]=true;
-         else
-        Hit[4]=false;
-        if (Physics2D.Raycast(transform.position,Vector3.down , range,floor ))
-        Hit[5]=true;
-         else
-        Hit[5]=false;
-        if (Physics2D.Raycast(transform.position, myVector2 = new Vector3(-0.5f, -0.5f, 0.0f) , range,floor ))
-        Hit[6]=true;
-         else
-        Hit[6]=false;
-        if (Physics2D.Raycast(transform.position,Vector3.left , range,floor ))
-        Hit[7]=true;
-         else
-        Hit[7]=false;
-        if (Physics2D.Raycast(transform.position, myVector3 = new Vector3(-0.5f, 0.5f, 0.0f) , range,floor ))
-        Hit[8]=true;
-         else
-        Hit[8]=false;
     }
 
+    [ContextMenu("Reset")]
+    public void Restart()
+    {
+        Start();
+    }
 }

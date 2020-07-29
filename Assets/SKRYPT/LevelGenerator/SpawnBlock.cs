5138c03d2739c6d4ae08c0869f9867c0043012a9
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-07-29 16:27:36 +0200

    wiecej blokow do generatora

--- Assets/SKRYPT/LevelGenerator/SpawnBlock.cs
+++ Assets/SKRYPT/LevelGenerator/SpawnBlock.cs
@@ -13,11 +13,14 @@ public class SpawnBlock : MonoBehaviour
     public GameObject[] Sky;
     public GameObject[] SkyCorner;
 
+    public GameObject Klocek;
+
     public LayerMask floor;
     public GameObject SpawnPoint;
     public float range;
     public bool[] Hit = new bool [10];
-    bool corner;
+    bool corner=false;
+    bool klocek = false;
     public int Level=1;
     public int Magnitude;
      Vector3 myVector;
@@ -64,13 +67,33 @@ public class SpawnBlock : MonoBehaviour
             }
             if ( Magnitude == 1)
             {
-                int rand = Random.Range(0, Sky.Length);
-                if (Sky.Length != 0)
+                if ( corner == true)
                 {
-                    if(Sky[rand] != null)
+                    int rand = Random.Range(0, SkyCorner.Length);
+                    if (Sky.Length != 0)
                     {
-                    GameObject obj = Instantiate(Sky[rand], transform.position, spawnRotation);
+                        if(Sky[rand] != null)
+                        {
+                        GameObject obj = Instantiate(SkyCorner[rand], transform.position, spawnRotation);
+                        obj.transform.parent = this.gameObject.transform;
+                        }
+                    }
+                }
+                else if ( klocek == true)
+                {
+                    GameObject obj = Instantiate(Klocek, transform.position, spawnRotation);
                     obj.transform.parent = this.gameObject.transform;
+                }
+                else 
+                {
+                    int rand = Random.Range(0, Sky.Length);
+                    if (Sky.Length != 0)
+                    {
+                        if(Sky[rand] != null)
+                        {
+                        GameObject obj = Instantiate(Sky[rand], transform.position, spawnRotation);
+                        obj.transform.parent = this.gameObject.transform;
+                        }
                     }
                 }
             }
@@ -108,6 +131,7 @@ public class SpawnBlock : MonoBehaviour
     }
     private void Rotacja()
     {
+        // normalne
          if (Hit[1]==false && Hit[3]==true && Hit[5]==true && Hit[7]==true  )
          {
              spawnRotation = Quaternion.Euler(0,0,0);
@@ -128,6 +152,40 @@ public class SpawnBlock : MonoBehaviour
              spawnRotation = Quaternion.Euler(0,0,90);
              return;
          }
+
+         // cornery 
+         else if (Hit[2]==false && Hit[4]==true && Hit[6]==true && Hit[8]==true && Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==true)
+         {
+             spawnRotation = Quaternion.Euler(0,0,0);
+             corner = true;
+             return;
+         }
+         else if (Hit[2]==true && Hit[4]==false && Hit[6]==true && Hit[8]==true && Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==true)
+         {
+             spawnRotation = Quaternion.Euler(0,0,270);
+             corner = true;
+             return;
+         }
+         else if (Hit[2]==true && Hit[4]==true && Hit[6]==false && Hit[8]==true && Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==true)
+         {
+             spawnRotation = Quaternion.Euler(0,0,180);
+             corner = true;
+             return;
+         }
+         else if (Hit[2]==true && Hit[4]==true && Hit[6]==true && Hit[8]==false && Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==true )
+         {
+             spawnRotation = Quaternion.Euler(0,0,90);
+             corner = true;
+             return;
+         }
+         else if (Hit[2]==true && Hit[4]==true && Hit[6]==true && Hit[8]==true && Hit[1]==true && Hit[3]==true && Hit[5]==true && Hit[7]==true )
+         {
+             spawnRotation = Quaternion.Euler(0,0,0);
+             klocek = true;
+             return;
+         }
+
+
 
 
 

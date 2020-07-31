38528a5c3ccbfede9a702c0e34fd92d9411d966c
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-07-31 19:35:16 +0200

    Spawnowanie swiatla

---
+++ Assets/SKRYPT/LevelGenerator/SpawnLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLight : MonoBehaviour
+{
+    public GameObject[] Lights;
+    
+    void Start()
+    {
+        if (transform.position.y < -10 )
+        {
+            Instantiate(Lights[Random.Range(0, Lights.Length)],transform.position, Quaternion.identity );
+        }
+    }
+
+    
+}

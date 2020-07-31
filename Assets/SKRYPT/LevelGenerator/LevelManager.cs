83d3546c532d60bdb1934638f86afeadc3e091a6
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-07-31 14:00:23 +0200

    Poprawki do generowania 2go poziomu

--- Assets/SKRYPT/LevelGenerator/LevelManager.cs
+++ Assets/SKRYPT/LevelGenerator/LevelManager.cs
@@ -16,8 +16,9 @@ public class LevelManager : MonoBehaviour
     void OnBossDied()
     {
         deleter.Delete();
+        GameController.instance.DataStorage.PlayerInfo.level = 2;
         //tutaj dać rzeczy z karmą i obecnym poziomem żeby zdecydować jaką wartość przypisać GameController.instance.DataStorage.PlayerInfo.level
         levelGen.Create();
-        player.transform.position = Vector2.zero;
+        player.transform.position = new Vector2(SaveSystem.Instance.levelGen.StartingPosition.x - SaveSystem.Instance.levelGen.moveAmountx, SaveSystem.Instance.levelGen.StartingPosition.y);
     }
 }

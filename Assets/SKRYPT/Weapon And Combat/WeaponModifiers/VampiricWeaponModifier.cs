bda87b6f63eb746fe5a9522d814d2995d2808e74
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-08-16 20:07:25 +0200

    nowy przedrostek

---
+++ Assets/SKRYPT/Weapon And Combat/WeaponModifiers/VampiricWeaponModifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Vampiric", menuName = "WeaponModifiers/Vampiric")]
+public class VampiricWeaponModifier : BaseWeaponModifier
+{
+    void OnPlayerDealDamage(float damage)
+    {
+        GameController.instance.DataStorage.PlayerInfo.currenthealth+=damage;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

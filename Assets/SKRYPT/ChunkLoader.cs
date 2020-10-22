90b929069f28e19daaa9119c247952c356d94a03
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-10-22 17:03:02 +0200

    Teoretycznie optymalizacja
    ukrywam chunki ktorych gracz nie widzi

---
+++ Assets/SKRYPT/ChunkLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkLoader : MonoBehaviour
+{
+    public GameObject[] ChunkComponents;
+    private GameObject player;
+    public float distance;
+    public float d2;
+    
+    void FixedUpdate()
+    {
+        player = GameObject.Find("Player");
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        if ( distance > d2)
+        {
+            foreach (GameObject chunkComponents in ChunkComponents)
+            {
+            chunkComponents.SetActive(false);
+            }
+        }
+        else  if ( distance < d2)
+        {
+            foreach (GameObject chunkComponents in ChunkComponents)
+            {
+            chunkComponents.SetActive(true);
+            }
+        }
+
+    }
+}

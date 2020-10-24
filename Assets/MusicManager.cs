840cbe95e3ba71276c2d59270663e3be7c12b35c
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-10-24 12:22:06 +0200

    muzyczka
    zmiana muzyki podczas gry

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioManager audioManager;
+    public Sound[] ThemeSounds;
+
+    // Start is called before the first frame update
+    public void Awake()
+    {
+        audioManager=AudioManager.instance;
+        
+
+
+        
+
+		ThemeSounds[0]=  Array.Find(audioManager.sounds, item => item.name == "Theme");
+        ThemeSounds[1]=  Array.Find(audioManager.sounds, item => item.name == "Theme2");
+        ThemeSounds[2]=  Array.Find(audioManager.sounds, item => item.name == "Theme3");
+	
+    }///  AudioSource.isPlaying
+    public void ChangeTheme1()
+    {
+        Invoke("ChangeTheme2",ThemeSounds[0].source.clip.length+1.0f);
+        Debug.Log("Theme1 sie skonczyl  ");
+    }
+    void ChangeTheme2()
+    {
+        Debug.Log("Theme2 zaczynam ");
+        AudioManager.instance.Play("Theme2");        
+        Invoke("ChangeTheme3",ThemeSounds[1].source.clip.length+1.0f);
+    }
+    public void ChangeTheme3()
+    {
+        Debug.Log("Theme3 zaczynam ");
+        AudioManager.instance.Play("Theme3");        
+        Invoke("ChangeTheme4",ThemeSounds[2].source.clip.length+1.0f);
+    }
+    public void ChangeTheme4()
+    {
+        AudioManager.instance.Play("Theme");    
+        Invoke("ChangeTheme2",ThemeSounds[0].source.clip.length+1.0f);
+        Debug.Log("Theme1 sie skonczyl  ");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

483594d0176c713314b4f378937919fe7b0468d8
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-10-25 17:55:50 +0100

    Wciśnięcie Graj w menu od razu zaczyna grę

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -16,32 +16,45 @@ public class MusicManager : MonoBehaviour
 
         
 
-		ThemeSounds[0]=  Array.Find(audioManager.sounds, item => item.name == "Theme");
-        ThemeSounds[1]=  Array.Find(audioManager.sounds, item => item.name == "Theme2");
-        ThemeSounds[2]=  Array.Find(audioManager.sounds, item => item.name == "Theme3");
+		ThemeSounds[0]=  Array.Find(audioManager?.sounds, item => item.name == "Theme");
+        ThemeSounds[1]=  Array.Find(audioManager?.sounds, item => item.name == "Theme2");
+        ThemeSounds[2]=  Array.Find(audioManager?.sounds, item => item.name == "Theme3");
+        //DontDestroyOnLoad(this.gameObject);
 	
     }///  AudioSource.isPlaying
     public void ChangeTheme1()
     {
-        Invoke("ChangeTheme2",ThemeSounds[0].source.clip.length+1.0f);
+        if(ThemeSounds[0].source != null)
+        {
+            Invoke("ChangeTheme2",ThemeSounds[0].source.clip.length+1.0f);
+        }
         Debug.Log("Theme1 sie skonczyl  ");
     }
     void ChangeTheme2()
     {
         Debug.Log("Theme2 zaczynam ");
-        AudioManager.instance.Play("Theme2");        
-        Invoke("ChangeTheme3",ThemeSounds[1].source.clip.length+1.0f);
+        AudioManager.instance.Play("Theme2");
+        if (ThemeSounds[1].source != null)
+        {
+            Invoke("ChangeTheme3", ThemeSounds[0].source.clip.length + 1.0f);
+        }
     }
     public void ChangeTheme3()
     {
         Debug.Log("Theme3 zaczynam ");
-        AudioManager.instance.Play("Theme3");        
-        Invoke("ChangeTheme4",ThemeSounds[2].source.clip.length+1.0f);
+        AudioManager.instance.Play("Theme3");
+        if (ThemeSounds[2].source != null)
+        {
+            Invoke("ChangeTheme4", ThemeSounds[2].source.clip.length + 1.0f);
+        }
     }
     public void ChangeTheme4()
     {
-        AudioManager.instance.Play("Theme");    
-        Invoke("ChangeTheme2",ThemeSounds[0].source.clip.length+1.0f);
+        AudioManager.instance.Play("Theme");
+        if (ThemeSounds[0].source != null)
+        {
+            Invoke("ChangeTheme2", ThemeSounds[0].source.clip.length + 1.0f);
+        }
         Debug.Log("Theme1 sie skonczyl  ");
     }
 
---
+++ Assets/SKRYPT/UI/StartGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private SaveSystem saveSystem;
+    [SerializeField] private LevelGeneration levelGen;
+    public void CheckSavefile()
+    {
+        int temp = SaveSystem.Instance.LoadGame();
+        switch (temp)
+        {
+            case 0:
+
+                LoadGame();
+
+                break;
+            case 1:
+
+                NewGame();
+
+                break;
+            case 2:
+
+                FirstGame();
+
+                break;
+        }
+    }
+
+    void LoadGame()
+    {
+        Debug.Log("StartGame :: Game was loaded");
+    }
+
+    void NewGame()
+    {
+        saveSystem.LoadPersistentData();
+        levelGen.Create();
+        Debug.Log("StartGame :: New Game was started");
+    }
+    void FirstGame()
+    {
+        levelGen.Create();
+        Debug.Log("StartGame :: New Game was started for the first time");
+    }
+
+
+}

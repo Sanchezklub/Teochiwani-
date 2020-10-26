c10740be44431ea30e4ba4701385065be99f4d85
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-10-26 20:30:07 +0100

    usuwanie zapisu w menu

---
+++ Assets/SKRYPT/UI/DeleteSavefile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DeleteSavefile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DeleteSave()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        File.Delete(path);
+    }
+}

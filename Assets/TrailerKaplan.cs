83e4d95e3782e026419a911296e07844991e6c84
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-10-26 17:02:17 +0100

    trailer

---
+++ Assets/TrailerKaplan.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerKaplan : MonoBehaviour
+{
+    public float PoIluSekundachAnimacje;
+    public Animator anim;
+    void Start()
+    {
+       StartCoroutine (StartAnimacji());
+    }
+
+    IEnumerator StartAnimacji()
+    {
+        
+        yield return new WaitForSeconds(PoIluSekundachAnimacje);
+        anim.SetBool("After8sekund", true);
+        
+    }
+}

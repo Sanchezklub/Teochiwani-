66f3bc28548a515e034fea5305437a947d39ddc1
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-10-27 10:32:35 +0100

    poczatek Xolotla

---
+++ Assets/SKRYPT/Oponenci/Xolotl/XolotlBrain.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class XolotlBrain : BaseBrain<XolotlBrain>
+{
+
+    public Animator enemyAnimator;
+    public BoxCollider2D boxCollider2D;
+    public Rigidbody2D rb2d;
+    
+    public UnityAction Attacking;
+    public UnityAction LeaveFightState;
+
+    private void Start()
+    {
+        
+        
+
+    }
+    private void Update()
+    {
+        UpdateChildState();
+    }
+
+    public override void ChangeState(BaseState<XolotlBrain> newState)
+    {
+        base.ChangeState(newState);
+        currentState?.DeinitState(this);
+        currentState = newState;
+        currentState?.InitState(this);
+    }
+
+    public override void UpdateChildState()
+    {
+        base.UpdateChildState();
+        currentState?.UpdateState();
+    }
+
+    public void StartAttackBlood()
+    {
+        ChangeState(new XolotlAttackStateBlood());
+    }
+
+    public void StartAttackDash()
+    {
+        //enemyToFollow = objectToFollow;
+        ChangeState(new XolotlAttackStateDash());
+    }
+    public void StartRest()
+    {
+        ChangeState(new XolotlRestState());
+    }
+    
+}
---
+++ Assets/SKRYPT/Oponenci/Xolotl/XolotlRestState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XolotlRestState : BaseState<XolotlBrain>
+{
+    private XolotlBrain brain;
+    public override void InitState(XolotlBrain controller)
+    {
+        base.InitState(controller);
+        this.brain = controller;
+    }
+    public override void UpdateState()
+    {
+        base.UpdateState();
+
+    }
+    public override void DeinitState(XolotlBrain controller)
+    {
+        
+        base.DeinitState(controller);
+    }
+}

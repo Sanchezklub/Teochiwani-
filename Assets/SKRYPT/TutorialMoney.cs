d6a2162d5055c57dd2c67f104c8ac395d237a8f3
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-11-01 17:30:54 +0100

    tutorial kolejne zmiany

---
+++ Assets/SKRYPT/TutorialMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMoney : MonoBehaviour
+{
+    public int kakao;
+    public int blood;
+    bool raz=false;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && !raz )
+        {
+            raz=true;
+            GameController.instance.DataStorage.PlayerInfo.cocoa = kakao;
+            GameController.instance.DataStorage.PlayerInfo.blood = blood;
+        }
+    }
+
+    
+}
---
+++ Assets/SKRYPT/UIHELPtutorial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIHELPtutorial : MonoBehaviour
+{
+    
+    public GameObject UIHelp;
+    public string Text;
+    public void Start()
+    {
+        UIHelp.GetComponent<TextMesh>().text = Text;
+    }
+    bool PlayerInRange;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            PlayerInRange = true;
+            UIHelp.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            UIHelp.SetActive(false);
+        }
+    }
+}

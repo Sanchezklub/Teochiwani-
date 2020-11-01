153d7bf5be6bfa001055672daf01a4d8f241a9e0
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-11-01 20:46:27 +0100

    tutorialowy portal (na razie tylko prefab)

---
+++ Assets/SKRYPT/Objects/TutorialPortal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.instance.DataStorage.PlayerInfo.portalPosition = transform.position;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}

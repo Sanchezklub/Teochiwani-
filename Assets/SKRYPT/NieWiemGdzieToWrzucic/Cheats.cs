0a8df1d4aa199a946f75d8af6414fc464d221e02
Sanchezklub <42272245+Sanchezklub@users.noreply.github.com>
2020-11-04 21:18:49 +0100

    cheaty dla Kacpra
    O i 1-9 spawnowanie broni oraz O i F1 - F4

--- Assets/SKRYPT/NieWiemGdzieToWrzucic/Cheats.cs
+++ Assets/SKRYPT/NieWiemGdzieToWrzucic/Cheats.cs
@@ -9,6 +9,7 @@ public class Cheats : MonoBehaviour
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject Rhino;
     [SerializeField] private GameObject Portal;
+    [SerializeField] private GameObject[] Weapon;
     void Update()
     {
         if (Input.GetKey(KeyCode.P))
@@ -36,6 +37,61 @@ public class Cheats : MonoBehaviour
             {
                 Player.GetComponent<Health>().TakeDamage(GameController.instance.DataStorage.PlayerInfo.currenthealth);
             }
-        }        
+        }   
+        if (Input.GetKey(KeyCode.O))
+        {
+           if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                 GameObject.Instantiate(Weapon[0], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }    
+             if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                 GameObject.Instantiate(Weapon[1], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }     
+             if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                 GameObject.Instantiate(Weapon[2], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }  
+             if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                 GameObject.Instantiate(Weapon[3], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }   
+             if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                 GameObject.Instantiate(Weapon[4], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }   
+             if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                 GameObject.Instantiate(Weapon[5], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }   
+             if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                 GameObject.Instantiate(Weapon[6], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }   
+             if (Input.GetKeyDown(KeyCode.Alpha8))
+            {
+                 GameObject.Instantiate(Weapon[7], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }    
+             if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                 GameObject.Instantiate(Weapon[8], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }   
+             if (Input.GetKeyDown(KeyCode.Alpha0))
+            {
+                 GameObject.Instantiate(Weapon[9], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }   
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                 GameObject.Instantiate(Weapon[10], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }  
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                 GameObject.Instantiate(Weapon[11], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }  
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                 GameObject.Instantiate(Weapon[12], GameController.instance.DataStorage.PlayerInfo.playerPosition, Quaternion.identity);
+            }  
+        }
     }
 }

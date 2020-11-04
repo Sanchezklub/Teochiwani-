c7cc5e607f65db409df6d1b65e6b36555afee22b
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-11-04 17:57:47 +0100

    evil player stoi jak nie ma odpowieniej broni + tweaki do ui

--- Assets/SKRYPT/Oponenci/EvilPlayer/EvilPlayerIdleState.cs
+++ Assets/SKRYPT/Oponenci/EvilPlayer/EvilPlayerIdleState.cs
@@ -17,7 +17,7 @@ public class EvilPlayerIdleState : BaseState<EvilPlayerBrain>
     {
         base.UpdateState();
         float dist = Vector3.Distance(GameController.instance.DataStorage.PlayerInfo.playerPosition, brain.transform.position);
-        if (dist < brain.AggroRange)
+        if (dist < brain.AggroRange && WeaponTypeCheck())
         {
             brain.StartFollow();
         }
@@ -27,5 +27,24 @@ public class EvilPlayerIdleState : BaseState<EvilPlayerBrain>
     {
         brain.animator.SetBool("Idle", false);
         base.DeinitState(controller);
+    }
+
+    bool WeaponTypeCheck()
+    {
+        if (brain.combat.currentWeapon == null)
+        {
+            return false;
+        }
+
+        else if (brain.combat.currentWeapon.AttackAnimationType == BaseWeapon.AnimationType.IsAttackingRanged || 
+            brain.combat.currentWeapon.AttackAnimationType == BaseWeapon.AnimationType.IsAttackingShoot ||
+            brain.combat.currentWeapon.AttackAnimationType == BaseWeapon.AnimationType.IsAttackingThrow)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }

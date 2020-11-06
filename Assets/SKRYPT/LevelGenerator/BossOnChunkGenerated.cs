1faecb0c33d2583aa014d7263c47c29033d3749b
JmateuszO <60017446+JmateuszO@users.noreply.github.com>
2020-11-06 17:19:04 +0100

    boss zależny od poziomu

---
+++ Assets/SKRYPT/LevelGenerator/BossOnChunkGenerated.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossOnChunkGenerated : MonoBehaviour
+{
+    public GameObject[] objects;
+    void Awake()
+    {
+        EventController.instance.levelEvents.OnChunkGeneratedBasic += SpawnObject;
+    }
+
+    void SpawnObject()
+    {
+        switch (GameController.instance.DataStorage.PlayerInfo.level)
+        {
+            case 0:
+                int rand = Random.Range(0, 2);
+                GameObject obj = Instantiate(objects[rand], transform.position, Quaternion.identity);
+                if (this.gameObject != null)
+                {
+                    obj.transform.parent = this.gameObject.transform;
+                }
+                break;
+            case 1:
+                GameObject obj1 = Instantiate(objects[3], transform.position, Quaternion.identity);
+                if (this.gameObject != null)
+                {
+                    obj1.transform.parent = this.gameObject.transform;
+                }
+                break;
+            case 2:
+                GameObject obj2 = Instantiate(objects[2], transform.position, Quaternion.identity);
+                if (this.gameObject != null)
+                {
+                    obj2.transform.parent = this.gameObject.transform;
+                }
+                break;
+        }
+
+    }
+    private void OnDestroy()
+    {
+        EventController.instance.levelEvents.OnChunkGeneratedBasic -= SpawnObject;
+    }
+}
